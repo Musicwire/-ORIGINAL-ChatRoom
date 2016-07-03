32e36eb3170710dcb5fe6baacaccf87cf78ab753
Musicwire <516710233@qq.com>
2016-07-03 23:17:50 +0800

    培耕的修改

--- Server_by_Bacon&WuYuan/tcpstreampackage.py
+++ Server_by_Bacon&WuYuan/tcpstreampackage.py
@@ -1,6 +1,7 @@
 import json
 from protocol import Protocol
 
+
 class TCPStreamPackage(object):
 
     def __init__(self, callback):
@@ -15,28 +16,24 @@ class TCPStreamPackage(object):
             self.datas = data
         else:
             self.datas = "%s%s" % (self.datas, data)
-
         #拆包
         length = -1
-        length_index = self.datas.find('length\" :')
+        length_index = self.datas.find('length\":')
         if length_index != -1:
-            length_end = self.datas.find(',', length_index + 9, length_index + 9 + 10)
+            length_end = self.datas.find(',', length_index + 8, length_index + 8 + 10)
             if length_end == -1:
-                length_end = self.datas.find('}', length_index + 9, length_index + 9 + 10)
+                length_end = self.datas.find('}', length_index + 8, length_index + 8 + 10)
 
             if length_end != -1:
-                length = self.datas[length_index + 9:length_end]
+                length = self.datas[length_index + 8:length_end]
 
         if length != -1:
             length = int(length.strip())
-
         if length != -1 and length <= len(self.datas):
            package = self.datas[0:length]
            package = Protocol.checkPackage(package)
-
            if self._package_decode_callback:
                self._package_decode_callback(package)
-
            if len(self.datas) == length:
                 self.datas = ''
            else:
