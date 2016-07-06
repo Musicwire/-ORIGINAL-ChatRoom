c2e7ae6caf18e14e97966ca44a08db81d398c699
Musicwire <516710233@qq.com>
2016-07-06 11:55:59 +0800

    培耕的修改

--- Server_by_Bacon_WuYuan/db.py
+++ Server_by_Bacon_WuYuan/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy import Column, Integer, String, Date, DateTime, ForeignKey
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
 Base = declarative_base()
 
